58759aa5ee5f566e050129b6bf017b367e31b618
doandadr <ddoanda@gmail.com>
2024-10-22 21:35:21 +0700

    initial commit

---
+++ android/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/android/AndroidLauncher.kt
@@ -0,0 +1,18 @@
+package com.github.doandadr.petualanganhijaiyah.android
+
+import android.os.Bundle
+
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import com.github.doandadr.petualanganhijaiyah.Main
+
+/** Launches the Android application. */
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initialize(Main(), AndroidApplicationConfiguration().apply {
+            // Configure your application here.
+            useImmersiveMode = true // Recommended, but not required.
+        })
+    }
+}
