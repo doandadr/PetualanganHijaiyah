548af1328fcc5bceeabca4582e270cd8ff785a2e
doandadr <ddoanda@gmail.com>
2024-10-26 10:56:14 +0700

    setup assets

--- core/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/Main.kt
+++ core/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/Main.kt
@@ -1,17 +1,48 @@
 package com.github.doandadr.petualanganhijaiyah
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
+import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.FitViewport
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
 import ktx.assets.disposeSafely
 import ktx.assets.toInternalFile
 import ktx.async.KtxAsync
+import ktx.freetype.registerFreeTypeFontLoaders
 import ktx.graphics.use
+import ktx.log.logger
+
+const val UNIT_SCALE = 1/16f
+const val V_WIDTH_PIXELS = 360
+const val V_HEIGHT_PIXELS = 640
+const val V_WIDTH = 9
+const val V_HEIGHT = 16
+private val Logger = logger<Main>()
 
 class Main : KtxGame<KtxScreen>() {
+
+    val batch: Batch by lazy{SpriteBatch()}
+    val uiViewport = FitViewport(V_WIDTH_PIXELS.toFloat(), V_HEIGHT_PIXELS.toFloat())
+    val stage: Stage by lazy {
+        val result = Stage(uiViewport, batch)
+        Gdx.input.inputProcessor = result
+        result
+    }
+
+    fun initiateAssetManager(): AssetManager {
+        val assetManager = AssetManager()
+        assetManager.registerFreeTypeFontLoaders()
+        return assetManager
+    }
+
+
+
     override fun create() {
         KtxAsync.initiate()
 
@@ -23,6 +54,8 @@ class Main : KtxGame<KtxScreen>() {
 class FirstScreen : KtxScreen {
     private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
     private val batch = SpriteBatch()
+
+
 
     override fun render(delta: Float) {
         clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
