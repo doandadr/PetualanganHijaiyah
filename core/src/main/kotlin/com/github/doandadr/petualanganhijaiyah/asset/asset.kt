ef8231d4ac9ddc24faafb96c69ec6189835ee631
doandadr <ddoanda@gmail.com>
2024-11-17 11:04:45 +0700

    update graphics assets

--- core/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/asset/asset.kt
+++ core/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/asset/asset.kt
@@ -90,7 +90,17 @@ enum class TextureAsset(
     HOME("bg-home.png"),
     STAGE("bg-stage.png"),
     START("bg-start.png"),
-    MAP("bg-map.png")
+    MAP("bg-map.png"),
+    SPOOKY_1("bg-spooky1.jpg"),
+    SPOOKY_2("bg-spooky2.jpg"),
+    FOREST_1("bg-forest1.jpg"),
+    FOREST_2("bg-forest2.jpg"),
+    DESERT_1("bg-desert1.jpg"),
+    DESERT_2("bg-desert2.jpg"),
+    SNOW_1("bg-snow1.jpg"),
+    SNOW_2("bg-snow2.jpg"),
+    MOUNTAIN_1("bg-mountain1.jpg"),
+    MOUNTAIN_2("bg-mountain2.jpg"),
 }
 
 enum class TextureAtlasAsset(
