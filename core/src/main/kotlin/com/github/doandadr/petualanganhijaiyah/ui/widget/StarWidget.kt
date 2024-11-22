b42826f876392e26d5c044004fd31ee3e23e5c0f
doandadr <ddoanda@gmail.com>
2024-11-22 21:28:04 +0700

    finish star graphics

--- core/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/ui/widget/StarWidget.kt
+++ core/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/ui/widget/StarWidget.kt
@@ -3,6 +3,7 @@ package com.github.doandadr.petualanganhijaiyah.ui.widget
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup
+import com.badlogic.gdx.utils.Align
 import ktx.scene2d.KGroup
 import ktx.scene2d.KWidget
 import ktx.scene2d.Scene2DSkin
@@ -23,12 +24,18 @@ class Stars(
     val star3: Image = Image(skin.getDrawable("star-grey")),
 ):WidgetGroup(star1, star2, star3), KGroup {
     init {
+        isTransform = true
         setInitPosition()
+        setState(StarState.ZERO)
+        setOrigin(Align.center)
+        setScale(0.8f)
+        star2.setOrigin(Align.center)
+        star2.setScale(1.1f)
     }
 
     private fun setInitPosition() {
         val bumpY = 30f
-        val gapX = 30f
+        val gapX = -10f
         star2.setPosition(star1.width + gapX, bumpY)
         star3.setPosition(star1.width + star2.width + 2 * gapX, star1.y)
     }
