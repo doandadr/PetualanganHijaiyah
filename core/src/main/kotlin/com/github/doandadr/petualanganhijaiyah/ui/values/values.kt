9ba15d838e4f58828b89db7e2caae1e273d2e3ea
doandadr <ddoanda@gmail.com>
2024-11-29 16:35:14 +0700

    revamp entire ui

---
+++ core/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/ui/values/values.kt
@@ -0,0 +1,11 @@
+package com.github.doandadr.petualanganhijaiyah.ui.values
+
+
+// Constants for variables
+const val SCALE_FONT_SPLASH = 3f
+const val SCALE_BTN_BIG = 1.5f
+const val SCALE_FONT_SMALL = 0.8f
+const val SCALE_BTN_MEDIUM = 1.2f
+const val SCALE_BTN_SMALL = 0.9f
+const val SCALE_FONT_BIG = 1.75f
+const val SCALE_FONT_MEDIUM = 1.25f
