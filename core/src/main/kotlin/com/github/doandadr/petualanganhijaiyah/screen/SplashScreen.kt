1e7589c5b9a2b298629989ffcdde59cb166e96c0
doandadr <ddoanda@gmail.com>
2024-12-11 04:49:42 +0700

    add timer and adjust UI of splash screen

--- core/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/screen/SplashScreen.kt
+++ core/src/main/kotlin/com/github/doandadr/petualanganhijaiyah/screen/SplashScreen.kt
@@ -1,25 +1,33 @@
 package com.github.doandadr.petualanganhijaiyah.screen
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.Scaling
+import com.badlogic.gdx.utils.Timer
 import com.github.doandadr.petualanganhijaiyah.Main
-import com.github.doandadr.petualanganhijaiyah.asset.SoundAsset
-import com.github.doandadr.petualanganhijaiyah.asset.TextureAsset
-import com.github.doandadr.petualanganhijaiyah.asset.TextureAtlasAsset
+import com.github.doandadr.petualanganhijaiyah.asset.*
+import com.github.doandadr.petualanganhijaiyah.ui.values.SCALE_FONT_SMALL
+import com.github.doandadr.petualanganhijaiyah.ui.values.SCALE_FONT_SPLASH
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
-import ktx.app.KtxScreen
+import ktx.assets.disposeSafely
 import ktx.async.KtxAsync
 import ktx.collections.gdxArrayOf
 import ktx.log.logger
+import ktx.scene2d.actors
+import ktx.scene2d.image
+import ktx.scene2d.label
+import ktx.scene2d.table
 
 
-private val LOG = logger<SplashScreen>()
-
-class SplashScreen(val game: Main) : KtxScreen {
+class SplashScreen(game: Main) : BaseScreen(game) {
+    private lateinit var bgSplash: Texture
 
     override fun show() {
+        super.show()
         val old = System.currentTimeMillis()
 
-        // queue asset loading
         val assetRefs = gdxArrayOf(
             TextureAsset.entries.map {
                 game.assets.loadAsync(it.descriptor)
@@ -29,30 +37,69 @@ class SplashScreen(val game: Main) : KtxScreen {
 
         ).flatten()
 
-        // once assets are loaded -> change to HomeScreen
         KtxAsync.launch {
             assetRefs.joinAll()
-            LOG.debug { "Time for assets to be loaded: ${System.currentTimeMillis() - old} ms" }
-            assetsLoaded()
+            log.debug { "Time for assets to be loaded: ${System.currentTimeMillis() - old} ms" }
+            Timer.schedule(object : Timer.Task() {
+                override fun run() {
+                    loadScreens()
+                }
+            }, SPLASH_DELAY_SECONDS)
         }
 
         setupUI()
     }
 
     private fun setupUI() {
-        // TODO Loading Screen UI
-        // TODO wait 1 second
+        bgSplash = Texture(Gdx.files.internal("graphics/bg/bg-home.png"))
+
+        stage.actors {
+            image(bgSplash) {
+                setFillParent(true)
+                setScaling(Scaling.fill)
+            }
+            table {
+                setFillParent(true)
+
+
+                image(Drawables.BISMILLAH_WHITE.drawable) {
+                    setOrigin(Align.center)
+                }
+
+                row()
+                label("PETUALANGAN\nHIJAIYAH", Labels.PRIMARY_GREEN_WHITE_BORDER.style) {
+                    setAlignment(Align.center)
+                    setFontScale(SCALE_FONT_SPLASH)
+                    it.spaceTop(20f)
+                }
+
+                row()
+                label("ayo berjelajah sambil belajar hijaiyah!", Labels.SECONDARY_GREEN_WHITE_BORDER.style) {
+                    setAlignment(Align.center)
+                    setFontScale(SCALE_FONT_SMALL)
+                    it.padBottom(300f)
+                }
+            }
+        }
     }
 
-
-
-    private fun assetsLoaded() {
+    private fun loadScreens() {
         game.addScreen(HomeScreen(game))
         game.addScreen(MapScreen(game))
-//        game.addScreen(LevelScreen(game)) TODO test levelScreen transition
+        game.addScreen(LevelScreen(game))
         game.addScreen(PracticeScreen(game))
+
         game.setScreen<HomeScreen>()
         game.removeScreen<SplashScreen>()
         dispose()
+    }
+
+    override fun dispose() {
+        bgSplash.disposeSafely()
+    }
+
+    companion object {
+        private const val SPLASH_DELAY_SECONDS = 1f
+        private val log = logger<SplashScreen>()
     }
 }
